ab702c4204d5d6a7c5be3c98709f5f4746bdeb3e
Matt Herich <mjherich@gmail.com>
2019-07-12 09:15:22 -0700

    Complete Card component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,52 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(data => {
+        console.log('SUCCESS: ', data)
+        const articleCategories = data.data.articles
+        console.log('Article Categories: ', articleCategories)
+        for (let articleCategory in articleCategories) {
+            console.log('category', articleCategory)
+            console.log(articleCategories[articleCategory])
+            articleCategories[articleCategory].forEach(article => {
+                const card = Card(article)
+                // Append each article card to .cards-container
+                document.querySelector('.cards-container').appendChild(card)
+            })
+        }
+    })
+    .catch(error => {
+        console.log('ERROR: ', error)
+    })
+
+function Card(articleData) {
+    // Create individual component elements
+    const card = document.createElement('div')
+    const headline = document.createElement('div')
+    const authorContainer = document.createElement('div')
+    const imgContainer = document.createElement('div')
+    const authorImg = document.createElement('img')
+    const authorName = document.createElement('span')
+
+    // Add classes to elements
+    card.classList.add('card')
+    headline.classList.add('headline')
+    authorContainer.classList.add('author')
+    imgContainer.classList.add('img-container')
+
+    // Apply data from api
+    headline.textContent = articleData.headline
+    authorImg.src = articleData.authorPhoto
+    authorName.textContent = articleData.authorName
+
+    // Stitch together component then return it
+    card.appendChild(headline)
+    card.appendChild(authorContainer)
+    authorContainer.appendChild(imgContainer)
+    imgContainer.appendChild(authorImg)
+    authorContainer.appendChild(authorName)
+    console.log('card: ', card)
+    return card
+}
