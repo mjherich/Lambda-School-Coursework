dd1bd96bc8ec50719ebb4f3dfadcd9accac144e7
Matt Herich <mjherich@gmail.com>
2019-08-13 14:18:45 -0700

    Create react app and initialize reducer

---
+++ src/reducers/todoReducer.js
@@ -0,0 +1,10 @@
+export const initialState = {
+  item: 'Learn about reducers',
+  completed: false,
+  id: 3892987589
+}
+
+const todoReducer = (state, action) => {
+  return { ...state }
+}
+export default todoReducer
