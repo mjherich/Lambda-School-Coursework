981eee0f9b58d493a39e411a5ea394319d9e0d66
Matt Herich <mjherich@gmail.com>
2019-08-14 13:57:49 -0700

    Create redux store and default reducer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { carStateReducer } from './reducers';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+export const store = createStore(carStateReducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+  </Provider>,
+  rootElement
+);
