dd5838d2e97539abc22fb65208f0b5d161523a5b
Matt Herich <mjherich@gmail.com>
2019-08-15 13:46:42 -0700

    Set up default reducer and connect store in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { qrReducer } from './reducers';
+
 import App from './App';
+
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create store
+export const store = createStore(qrReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/reducers/qrReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  str: 'iniital state',
+  generatedCodes: []
+}
+
+export const qrReducer = ( state = initialState, action ) => {
+  switch (action.type) {
+    default:
+      return { ... state }
+  }
+}
