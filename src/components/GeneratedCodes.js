d69e4ab1280aff25ad56312ecc21cb313d166504
Matt Herich <mjherich@gmail.com>
2019-08-15 16:15:30 -0700

    Connect qrCode state to UI

--- src/components/GeneratedCodes.js
+++ src/components/GeneratedCodes.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Divider } from 'semantic-ui-react';
 
-export default function GeneratedCodes() {
+function GeneratedCodes(props) {
   return (
     <>
-      <h2>Generated Code goes here</h2>
-      <img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example" alt="generated qr code" />
+      <h2>Generated QR Codes</h2>
+      {props.qrCodes.length===0 ? (
+        <p>No Generated QR codes</p>
+      ) : (
+        <>
+          <img src={`https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${encodeURI(props.qrCodes[0].qrString)}`} alt="generated qr code" />
+          <p>{props.qrCodes[0].qrString}</p>
+        </>
+      )}
       <Divider horizontal>Previously Generated Codes</Divider>
-      <img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example" alt="generated qr code" />
+      {props.qrCodes.length>1 && props.qrCodes.map((qrCode, idx) => {
+        const encodedStr =  encodeURI(qrCode.qrString)
+        return (
+          <img src={`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodedStr}`} alt="generated qr code" />
+        )
+      })}
     </>
   )
 }
+
+const mapStateToProps = state => {
+  return {
+    qrCodes: state.qrCodes,
+    isLoading: state.isLoading
+  }
+}
+export default connect(
+  mapStateToProps,
+  {}
+)(GeneratedCodes)
