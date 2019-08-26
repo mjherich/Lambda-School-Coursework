23f534b1c873c347e1ae5d34ea377923484235ee
Eleasah Halsmer <eleasahjh@gmail.com>
2019-08-26 15:17:05 -0600

    Using semantic ui cards

--- src/userCard.js
+++ src/userCard.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Card } from 'semantic-ui-react'
+
 
 
 const UserCard = (props) => {
-    // handleClick function. Should route to userPage by pushing that onto history
+    console.log('props in userCard', props)
+    // TODO: handleClick function. Should route to userPage by pushing that onto history. Or, wrap in Link. 
+
     return (
-        <div>Username: {props.user.username} Salty Score: {props.user.saltyScore}</div>
+        <Card fluid>
+            <div className="userCard">
+                <div>{props.user.username}</div>
+                <div>Salty Score: {props.user.saltyScore}</div>
+            </div>
+            {/* <img src={`https://robohash.org/${props.user.username}.png?set=set5`}/> */}
+        </Card>
     )
 }
+
+
 
 export default UserCard;
--- src/userList.js
+++ src/userList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import UserCard from './userCard';
+import { Container, Card } from 'semantic-ui-react';
 
 const UserList = props => {
   // useState to store user list
@@ -11,25 +12,24 @@ const UserList = props => {
     axios
       .get("https://my.api.mockaroo.com/topsaltyusers.json?key=917e1550")
       .then(response => {
-        console.log("get response", response.data);
+        // console.log("get response", response.data);
         const array = response.data;
         let sortedBySalt = response.data.sort((a, b) => {
           return b.saltyScore - a.saltyScore;
         });
-        console.log("sortedBySalt", sortedBySalt);
+        // console.log("sortedBySalt", sortedBySalt);
         setUsers(sortedBySalt);
       });
   }, []);
 
-  // return .map over list of users, rendering a card for each
-
+  // return .map over list of users, rendering a UserCard for each
   return (
-    <div>
-      <h1>100 Saltiest Users</h1>
-      {users.map(user => {
-        return <UserCard user={user} />;
-      })}
-    </div>
+      <Card.Group itemsPerRow='1'>
+        {/* <h1>100 Saltiest Users</h1> */}
+        {users.map(user => {
+          return <UserCard user={user}/>;
+        })}
+      </Card.Group>
   );
 };
 
