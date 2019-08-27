32289348c8a51201e4792df76d5b5357cec0c284
leehanchung <kein520@yahoo.com>
2019-08-26 23:16:37 -0700

    wordcloud bytesio

--- data/app.py
+++ data/app.py
@@ -5,7 +5,8 @@ import psycopg2
 from flask import Flask, json, jsonify, request, send_file
 from wordcloud import WordCloud
 from PIL import Image
-from io import StringIO
+#from io import StringIO
+from io import BytesIO
 
 
 app = Flask(__name__)
@@ -92,7 +93,8 @@ def user_wordcloud():
     return img
     
 def serve_pil_image(pil_img):
-    img_io = StringIO()
+    #img_io = StringIO()
+    img_io = BytesIO()
     pil_img.save(img_io, 'JPEG', quality=70)
     img_io.seek(0)
     return send_file(img_io, mimetype='image/jpeg')
