86d46bcfc82f02bc6ce7728aee4787e3f0bee5aa
Matt Herich <mjherich@gmail.com>
2019-09-11 14:55:26 -0700

    Update models

--- javaorders/src/main/java/com/lambdaschool/javaorders/models/Order.java
+++ javaorders/src/main/java/com/lambdaschool/javaorders/models/Order.java
@@ -1,5 +1,7 @@
 package com.lambdaschool.javaorders.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 
 @Entity
@@ -16,4 +18,83 @@ public class Order
 
     @Column(nullable = false)
     private long custcode;
+
+    @ManyToOne
+    @JoinColumn(name = "custcode",
+            nullable = false)
+    @JsonIgnoreProperties("orders")
+    private Customer customer;
+
+    public Order()
+    {
+    }
+
+    public Order(double ordamount, double advanceamount, String orddescription, long custcode, Customer customer)
+    {
+        this.ordamount = ordamount;
+        this.advanceamount = advanceamount;
+        this.orddescription = orddescription;
+        this.custcode = custcode;
+        this.customer = customer;
+    }
+
+    public long getOrdnum()
+    {
+        return ordnum;
+    }
+
+    public void setOrdnum(long ordnum)
+    {
+        this.ordnum = ordnum;
+    }
+
+    public double getOrdamount()
+    {
+        return ordamount;
+    }
+
+    public void setOrdamount(double ordamount)
+    {
+        this.ordamount = ordamount;
+    }
+
+    public double getAdvanceamount()
+    {
+        return advanceamount;
+    }
+
+    public void setAdvanceamount(double advanceamount)
+    {
+        this.advanceamount = advanceamount;
+    }
+
+    public String getOrddescription()
+    {
+        return orddescription;
+    }
+
+    public void setOrddescription(String orddescription)
+    {
+        this.orddescription = orddescription;
+    }
+
+    public long getCustcode()
+    {
+        return custcode;
+    }
+
+    public void setCustcode(long custcode)
+    {
+        this.custcode = custcode;
+    }
+
+    public Customer getCustomer()
+    {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer)
+    {
+        this.customer = customer;
+    }
 }
