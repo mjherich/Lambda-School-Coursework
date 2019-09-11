8a89e631e113dde819b5e4d8b0001a762466a795
Matt Herich <mjherich@gmail.com>
2019-09-11 16:06:27 -0700

    Finish GET endpoints in controller

--- javaorders/src/main/java/com/lambdaschool/javaorders/controllers/CustomersController.java
+++ javaorders/src/main/java/com/lambdaschool/javaorders/controllers/CustomersController.java
@@ -1,24 +1,29 @@
 package com.lambdaschool.javaorders.controllers;
 
+import com.lambdaschool.javaorders.models.Customer;
 import com.lambdaschool.javaorders.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class CustomersController
 {
     @Autowired
-    private CustomersService customersService;
+    private CustomerService customerService;
 
     // GET http://localhost:2019/customer/order - Returns all customers with their orders
     @GetMapping(value = "/customer/order",
             produces = {"application/json"})
     public ResponseEntity<?> listAllOrders()
     {
-
+        List<Customer> customers = customerService.findAll();
+        return new ResponseEntity<>(customers, HttpStatus.OK);
     }
 
     // GET http://localhost:2019/customer/name/{custname} - Returns all orders for a particular customer based on name
@@ -28,7 +33,8 @@ public class CustomersController
             @PathVariable
                     String custname)
     {
-        Customer c = customersService.find
+        Customer c = customerService.findByCustomerName(custname);
+        return new ResponseEntity<>(c, HttpStatus.OK);
     }
 
     // POST http://localhost:2019/data/customer/new - Adds a new customer including any new orders
--- javaorders/src/main/java/com/lambdaschool/javaorders/repos/OrdersRepository.java
+++ javaorders/src/main/java/com/lambdaschool/javaorders/repos/OrdersRepository.java
@@ -3,6 +3,6 @@ package com.lambdaschool.javaorders.repos;
 import com.lambdaschool.javaorders.models.Order;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OrdersRepository extends CrudRepository<Order, long>
+public interface OrdersRepository extends CrudRepository<Order, Long>
 {
 }
