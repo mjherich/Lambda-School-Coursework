90f46c4a45248d5f2fb16f1cf513d8ca87c2817c
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-23 13:54:19 -0400

    made helper dashboard

---
+++ devdesk/src/components/Helper/HelperDashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import TicketList from '../Ticket/TicketList';
+
+const HelperDashboard = () => {
+
+    return (
+        <div>
+            <h2>Helper Dashboard</h2>
+            <TicketList />
+        </div>
+    )
+}
+
+export default HelperDashboard;
