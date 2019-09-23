73d70d664948f5095a294bf7f4e2152a446b4150
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-23 14:46:50 -0400

    set up reducer and put provider wrapper around app

--- devdesk/src/index.js
+++ devdesk/src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
+import { reducer } from './store/reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>
     , document.getElementById('root'));
---
+++ devdesk/src/store/reducers/index.js
@@ -0,0 +1,16 @@
+const initialState = {
+    userType: '',
+    getError: '',
+    postError: '',
+    putError: '',
+    isGetting: false,
+    isPosting: false,
+    isPutting: false,
+}
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
