bf7a75929c41a81763f15156407ee62a67dadd28
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-24 19:11:38 -0400

    filled out axios request in add ticket

--- devdesk/src/components/Student/AddTicket.js
+++ devdesk/src/components/Student/AddTicket.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import axiosWithAuth from '../../utils/axiosWithAuth';
+
 const initialTicket = {
     id: '',
     title: '',
@@ -24,31 +26,36 @@ const AddTicket = () => {
     const handleSubmit = (e) => {
         //axiosWithAuth post request here after adding StudentID and randomly assigning ticket id
         e.preventDefault();
-        setTicket(initialTicket);
-    }
+        axiosWithAuth().post('url', {
+            ...ticket, id: Date.now(), student: //grab from store })
+            .then(res => console.log(res))
+                    .catch(err => console.log('AddTicket.js: Post: ', err));
+            //need to move this to actions folder
+            setTicket(initialTicket);
+        }
 
     return (
-        <div className='form-div'>
-            <h2>Add Ticket Form</h2>
-            <form onSubmit={handleSubmit}>
-                <label>Title</label>
-                <input className='inputs' type='text' name='title' placeholder='Title' value={ticket.title} onChange={handleChange} />
-                <label>Category</label>
-                <select className='inputs' name='category' onChange={handleChange} value={ticket.category}>
-                    <option>Select a category</option>
-                    <option value="html">HTML</option>
-                    <option value="css">CSS</option>
-                    <option value="javascript">Javascript</option>
-                    <option value="react">React</option>
-                    <option value="redux">Redux</option>
-                    <option value="other">Other</option>
-                </select>
-                <label>Description</label>
-                <textarea className='inputs' name='description' placeholder='How can I ....' onChange={handleChange} value={ticket.description} />
-                <button type='submit'>Submit Ticket</button>
-            </form>
-        </div>
-    )
-}
+            <div className='form-div'>
+                <h2>Add Ticket Form</h2>
+                <form onSubmit={handleSubmit}>
+                    <label>Title</label>
+                    <input className='inputs' type='text' name='title' placeholder='Title' value={ticket.title} onChange={handleChange} />
+                    <label>Category</label>
+                    <select className='inputs' name='category' onChange={handleChange} value={ticket.category}>
+                        <option>Select a category</option>
+                        <option value="html">HTML</option>
+                        <option value="css">CSS</option>
+                        <option value="javascript">Javascript</option>
+                        <option value="react">React</option>
+                        <option value="redux">Redux</option>
+                        <option value="other">Other</option>
+                    </select>
+                    <label>Description</label>
+                    <textarea className='inputs' name='description' placeholder='How can I ....' onChange={handleChange} value={ticket.description} />
+                    <button type='submit'>Submit Ticket</button>
+                </form>
+            </div>
+        )
+    }
 
-export default AddTicket;
+    export default AddTicket;
