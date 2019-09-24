012f173b19202fa7c59f30f22b3de3f0a765c35e
Jeremy Rogel <jeremyrogel@Jeremys-MBP.lan1>
2019-09-24 11:48:10 -0400

    login page

--- devdesk/src/App.js
+++ devdesk/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Login from './components/UserOnboarding/Login';
 
 function App() {
   return (
     <div className="App">
-      Test
+      <Login />
     </div>
   );
 }
