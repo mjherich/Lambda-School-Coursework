11212acbf041036542209a06621848f55a0bdacc
CarnunMP <carnun@hotmail.co.uk>
2019-09-25 13:48:55 +0100

    dropdown outline done

--- devdesk/src/components/Header/NavBar.js
+++ devdesk/src/components/Header/NavBar.js
@@ -1,44 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 const StyledNavBar = styled.div`
     width: 6rem;
     display: flex;
-    justify-content: space-between;
-    align-items: center;
+    flex-direction: column;
 
-    .menu {
-        padding-top: 0.5rem;
-    }
-
-    .hamburger {
-        height: 1.2rem;
-        width: 1.5rem;
+    .menu-and-hamburger {
         display: flex;
-        flex-direction: column;
         justify-content: space-between;
         align-items: center;
+        height: 100%;
 
-
-        div {
-            min-height: 3px;
-            width: 100%;
-            background: white;
+        .menu {
+            padding-top: 0.5rem;
         }
+
+        .hamburger {
+            height: 1.2rem;
+            width: 1.5rem;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            align-items: center;
+
+            div {
+                min-height: 3px;
+                width: 100%;
+                background: white;
+            }
+
+            cursor: pointer;
+        }
+    }
+
+    .dropdown {
+        background: darkgrey;
+        height: 25rem;
+        position: absolute;
+        top: 4rem;
+        right: 0;
+
+        &[dropdown="false"] {
+            width: 0;
+        }
+
+        &[dropdown="true"] {
+            width: 10rem;
+        }
+
+        transition: width 0.5s;
     }
 `;
 
 export default function NavBar(props) {
+    const [dropDown, setDropDown] = useState("false");
+
+    const toggleDropDown = () => {
+        dropDown === "true" ?
+        setDropDown("false") :
+        setDropDown("true");
+    };
+
     return (
         <StyledNavBar className="nav-bar">
-            <div className="menu">
-                <h5>MENU</h5>
+            <div className="menu-and-hamburger">
+                <div className="menu">
+                    <h5>MENU</h5>
+                </div>
+                <div className="hamburger" onClick={toggleDropDown}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
             </div>
-            <div className="hamburger">
-                <div></div>
-                <div></div>
-                <div></div>
+            <div className="dropdown" dropdown={dropDown}>
+
             </div>
         </StyledNavBar>
     )
