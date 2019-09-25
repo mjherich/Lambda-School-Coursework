c32db52db266b529de09499e5a6a35e077c3465b
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-25 13:22:04 -0400

    created postTicket action

--- devdesk/src/store/actions/index.js
+++ devdesk/src/store/actions/index.js
@@ -6,7 +6,18 @@ export const FETCH_TICKETS_FAIL = 'FETCH_TICKETS_FAIL';
 
 export const fetchTickets = () => dispatch => {
     dispatch({ type: FETCH_TICKETS_START });
-    axiosWithAuth().get()
+    axiosWithAuth().get('url')
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+}
+
+export const POST_TICKET_START = 'POST_TICKET_START';
+export const POST_TICKET_SUCCESS = 'POST_TICKET_SUCCESS';
+export const POST_TICKET_FAIL = 'POST_TICKET_FAIL';
+
+export const postTicket = (ticket) => dispatch => {
+    dispatch({ type: POST_TICKET_START });
+    axiosWithAuth().post('url', ticket)
         .then(res => console.log(res))
         .catch(err => console.log(err))
 }
