8d407094928a3f103e63e4bbf2e72240c5e74749
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-25 16:01:06 -0400

    added cases for reducer

--- devdesk/src/store/reducers/index.js
+++ devdesk/src/store/reducers/index.js
@@ -1,3 +1,9 @@
+import {
+    FETCH_TICKETS_START, FETCH_TICKETS_SUCCESS, FETCH_TICKETS_FAIL,
+    PUT_TICKET_START, POST_TICKET_START, POST_TICKET_SUCCESS, POST_TICKET_FAIL,
+    PUT_TICKET_SUCCESS, PUT_TICKET_FAIL
+} from '../actions'
+
 const initialState = {
     userType: '',
     user: '',
@@ -12,6 +18,62 @@ const initialState = {
 
 export const reducer = (state = initialState, action) => {
     switch (action.type) {
+        case FETCH_TICKETS_START:
+            return {
+                ...state,
+                isGetting: true,
+                getError: '',
+            };
+        case FETCH_TICKETS_SUCCESS:
+            return {
+                ...state,
+                isGetting: false,
+                ticketArray: ,
+                userType: ,
+                user: ,
+            };
+        case FETCH_TICKETS_FAIL:
+            return {
+                ...state,
+                isGetting: false,
+                getError: action.payload,
+            };
+        case POST_TICKET_START:
+            return {
+                ...state,
+                isPosting: true,
+                postError: '',
+            };
+        case POST_TICKET_SUCCESS:
+            return {
+                ...state,
+                isPosting: false,
+                ticketArray: ,
+            };
+        case POST_TICKET_FAIL:
+            return {
+                ...state,
+                isPosting: false,
+                postError: action.payload,
+            };
+        case PUT_TICKET_START:
+            return {
+                ...state,
+                isPutting: true,
+                putError: '',
+            };
+        case PUT_TICKET_SUCCESS:
+            return {
+                ...state,
+                isPutting: false,
+                ticketArray: ,
+            };
+        case PUT_TICKET_FAIL:
+            return {
+                ...state,
+                isPutting: false,
+                putError: action.payload,
+            };
         default:
             return state
     }
