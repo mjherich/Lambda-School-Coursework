139fe70432aca3d12e9a288f0c5573d2baf519c2
CarnunMP <carnun@hotmail.co.uk>
2019-09-26 09:39:24 +0100

    logo added

--- devdesk/src/components/Header/Header.js
+++ devdesk/src/components/Header/Header.js
@@ -3,17 +3,27 @@ import styled from "styled-components";
 
 import NavBar from "./NavBar";
 
+import logo from "../images/logo.png";
+
 const StyledHeader = styled.header`
-    background: slategrey;
+    background: #6A6A6A;
     height: 4rem;
     width: 100vw;
     display: flex;
     justify-content: space-between;
+    align-items: center;
     padding: 0 2rem 0 2rem;
 
     .logo {
-        border: 1px solid green;
-        width: 4rem;
+        width: 3.5rem;
+        height: 3.5rem;
+        border-radius: 10px;
+        overflow: hidden;
+
+        img {
+            height: 100%;
+            width: 100%;
+        }
     }
 `;
 
@@ -21,7 +31,7 @@ export default function Header(props) {
     return (
         <StyledHeader>
             <div className="logo">
-                [Logo]
+                <img src={logo} alt="logo" />
             </div>
             <NavBar />
         </StyledHeader>
