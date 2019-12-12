a94efcc53955c48a68992c061c79a33a0728e879
Matt Herich <mjherich@gmail.com>
2019-12-12 14:21:46 -0800

    Complete rps.py

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,23 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  moves = ['rock', 'paper', 'scissors']
+
+  # Array to hold each game permutation
+  games = []
+  
+  def find_games(n, turn=[]):
+    # Check if end of permutation and append to plays
+    if n == 0:
+      games.append(turn)
+      return
+
+    for move in moves:
+      find_games(n-1, turn + [move])
+
+  find_games(n)
+  return games
+
 
 
 if __name__ == "__main__":
