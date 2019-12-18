b94128936bd19b0f3487202be3478304ae29bbf5
Matt Herich <mjherich@gmail.com>
2019-12-18 13:05:35 -0800

    Implement bst insert, contains, get_max and for_each methods

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -10,23 +10,58 @@ class BinarySearchTree:
         self.left = None
         self.right = None
 
-    # Insert the given value into the tree
+    """
+    Insert the given value into the tree
+    """
     def insert(self, value):
-        pass
+        if not self.value:
+            self.value = value
+            return
 
-    # Return True if the tree contains the value
-    # False if it does not
+        if value < self.value and not self.left:
+            self.left = BinarySearchTree(value)
+        elif value < self.value and self.left:
+            self.left.insert(value)
+        elif value > self.value and not self.right:
+            self.right = BinarySearchTree(value)
+        elif value > self.value and self.right:
+            self.right.insert(value)
+
+    """
+    Return True if the tree contains the value
+    False if it does not
+    """
     def contains(self, target):
-        pass
+        if target == self.value:
+            return True
+        elif target < self.value and self.left:
+            return self.left.contains(target)
+        elif target < self.value and not self.left:
+            return False
+        elif target > self.value and self.right:
+            return self.right.contains(target)
+        elif target > self.value and not self.right:
+            return False
 
-    # Return the maximum value found in the tree
+    """
+    Return the maximum value found in the tree
+    """
     def get_max(self):
-        pass
+        if self.right:
+            return self.right.get_max()
+        else:
+            return self.value
 
-    # Call the function `cb` on the value of each node
-    # You may use a recursive or iterative approach
+    """
+    Call the function `cb` on the value of each node
+    You may use a recursive or iterative approach
+    """
     def for_each(self, cb):
-        pass
+        cb(self.value)
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
 
     # DAY 2 Project -----------------------
 
