e933ef2dfab27aee958158827b79815ff06b1be7
Matt Herich <mjherich@gmail.com>
2019-12-19 14:36:16 -0800

    Implement max_heap insert and _bubble_up methods. Also create helpers for getting parent and child indicies

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,9 +1,27 @@
+from math import floor
+
 class Heap:
     def __init__(self):
         self.storage = []
 
+    def get_parent_index(self, idx):
+        return floor((idx - 1) / 2)
+
+    def get_left_child_index(self, idx):
+        return floor(2 * idx + 1)
+
+    def get_right_child_index(self, idx):
+        return floor(2 * idx + 2)
+
     def insert(self, value):
-        pass
+        self.storage.append(value)
+        index = len(self.storage) - 1
+        parent_index = self.get_parent_index(index)
+        # If the parent value is less than the value we just added (end of array) we need to swap
+        while self.storage[parent_index] < self.storage[index] and parent_index >= 0:
+            self._bubble_up(index)
+            index = parent_index
+            parent_index = self.get_parent_index(index)
 
     def delete(self):
         pass
@@ -15,7 +33,8 @@ class Heap:
         pass
 
     def _bubble_up(self, index):
-        pass
+        parent_index = self.get_parent_index(index)
+        self.storage[index], self.storage[parent_index] = self.storage[parent_index], self.storage[index]
 
     def _sift_down(self, index):
         pass
