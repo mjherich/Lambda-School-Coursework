bb5e460e42544c801d7bfa6b594f7b77aee9027a
Matt Herich <mjherich@gmail.com>
2020-01-15 13:39:46 -0800

    Remove proof_of_work from server

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -80,20 +80,6 @@ class Blockchain(object):
     def last_block(self):
         return self.chain[-1]
 
-    def proof_of_work(self, block):
-        """
-        Simple Proof of Work Algorithm
-        Stringify the block and look for a proof.
-        Loop through possibilities, checking each one against `valid_proof`
-        in an effort to find a number that is a valid proof
-        :return: A valid proof for the provided block
-        """
-        block_string = json.dumps(block)
-        proof = 0
-        while self.valid_proof(block_string, proof) is False:
-            proof += 1
-        return proof
-
     @staticmethod
     def valid_proof(block_string, proof):
         """
