3c03370f8819eda0b4f5d6acb747cd5854f009bd
Matt Herich <mjherich@gmail.com>
2020-01-16 13:29:06 -0800

    Add login

--- basic_wallet_p/lambda-wallet/src/App.js
+++ basic_wallet_p/lambda-wallet/src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { createContext, useContext } from 'react';
 import './App.css';
+import Container from '@material-ui/core/Container';
+
+import AppContext from './state/AppContext'
+import Login from './components/Login';
 
 function App() {
+  const [state, setState] = React.useState({ id: "" });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppContext.Provider value={{state, setState}}>
+        <Container className="main-container">
+          {state.id === "" ? <Login /> : <p>Logged in as {state.id}</p>}
+        </Container>
+      </AppContext.Provider>
     </div>
   );
 }
