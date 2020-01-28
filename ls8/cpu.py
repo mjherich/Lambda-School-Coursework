c8a39a82330885a3e3b65ca33e32c0993936c595
Matt Herich <mjherich@gmail.com>
2020-01-28 15:23:36 -0800

    Implement the multiply instruction and add binary parsing to load()

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -11,11 +11,6 @@ class CPU:
                                   # R5: Interrupt Mask (IM), R6: Interrupt Status (IS), R7: Stack Pointer (SP)
         self.ram = [0] * 256      # Ram contains 256 bytes of memory
         self.pc = 0               # Program Counter, address of the currently executing instruction
-        self._instructions = {
-            "LDI": 0b10000010,
-            "PRN": 0b01000111,
-            "HLT": 0b00000001
-        }
 
     def load(self, filename):
         """Load a program into memory."""
@@ -30,7 +25,7 @@ class CPU:
                     if num == "":
                         continue
 
-                    value = int(num)
+                    value = eval(f"0b{num}")
 
                     self.ram_write(address, value)
                     address += 1
@@ -81,43 +76,26 @@ class CPU:
         halted = False
         while not halted:
             # Get the instruction from ram and store in local instruction register
-            IR = self.ram[PC]
+            IR = self.ram_read(PC)
             # Get operands
-            operand_a = self.ram[PC + 1]
-            operand_b = self.ram[PC + 2]
+            operand_a = self.ram_read(PC + 1)
+            operand_b = self.ram_read(PC + 2)
             # Run the correct operation
-            if IR == self._instructions["LDI"]:
+            if IR == 0b10000010:      # LDI
                 self.registers[operand_a] = operand_b
                 PC += 3
-            elif IR == self._instructions["PRN"]:
+            elif IR == 0b01000111:    # PRN
                 print(self.registers[operand_a])
                 PC += 2
-            elif IR == self._instructions["HLT"]:
+            elif IR == 0b10100010:    # MUL
+                product = self.registers[operand_a] * self.registers[operand_b]
+                self.registers[operand_a] = product
+                PC += 3
+            elif IR == 0b00000001:    # HLT
                 halted = True
             else:
                 raise Exception(f"Command {IR} does not exist")
 
-    def load_memory(self, filename):
-        try:
-            address = 0
-            with open(filename) as f:
-                for line in f:
-                    # Ignore comments
-                    comment_split = line.split("#")
-                    num = comment_split[0].strip()
-
-                    if num == "":
-                        continue
-
-                    value = int(num)
-
-                    self.ram_write(address, value)
-                    print("Ram Written")
-                    address += 1
-
-        except FileNotFoundError:
-            print(f"{sys.argv[0]}: {filename} not found")
-            sys.exit(2)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -125,3 +103,5 @@ if __name__ == '__main__':
         sys.exit(1)
     cpu = CPU()
     cpu.load(sys.argv[1])
+
+    cpu.run()
