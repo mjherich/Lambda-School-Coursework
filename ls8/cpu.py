f53118163eb20eddccffd602a437008e9676ce6c
Matt Herich <mjherich@gmail.com>
2020-01-29 13:25:22 -0800

    Implement branch_table for O(1) opcode lookup

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,15 +2,28 @@
 
 import sys
 
+# Opcodes for branch table
+LDI = 0b10000010
+PRN = 0b01000111
+MUL = 0b10100010
+HLT = 0b00000001
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        self.registers = [0] * 8  # List for storing registers R0 - R7
-                                  # R5: Interrupt Mask (IM), R6: Interrupt Status (IS), R7: Stack Pointer (SP)
-        self.ram = [0] * 256      # Ram contains 256 bytes of memory
-        self.pc = 0               # Program Counter, address of the currently executing instruction
+        self.registers = [0] * 8    # List for storing registers R0 - R7
+                                    # R5: Interrupt Mask (IM), R6: Interrupt Status (IS), R7: Stack Pointer (SP)
+        self.ram = [0] * 256        # Ram contains 256 bytes of memory
+        self.pc = 0                 # Program Counter, address of the currently executing instruction
+        self._halted = False        # Used for run(), set to False in HLT()
+        self.branch_table = {}
+        self.branch_table[LDI] = self.handle_LDI
+        self.branch_table[PRN] = self.handle_PRN
+        self.branch_table[MUL] = self.handle_MUL
+        self.branch_table[HLT] = self.handle_HLT
+
 
     def load(self, filename):
         """Load a program into memory."""
@@ -50,6 +63,27 @@ class CPU:
         else:
             raise Exception("Unsupported ALU operation")
 
+    def handle_LDI(self, operand_a, operand_b):
+        print(f"running LDI: store {operand_b} in register {operand_a}")
+        self.registers[operand_a] = operand_b
+        self.pc += 3
+
+    def handle_PRN(self, operand_a, operand_b):
+        print(f"running PRN: print register {operand_a}")
+        print(self.registers[operand_a])
+        self.pc += 2
+
+    def handle_MUL(self, operand_a, operand_b):
+        print(f"running MUL: multiply registers {operand_a} and {operand_b} and store result in {operand_a}")
+        product = self.registers[operand_a] * self.registers[operand_b]
+        self.registers[operand_a] = product
+        self.pc += 3
+
+    def handle_HLT(self, operand_a, operand_b):
+        print("running HLT")
+        self._halted = True
+
+
     def trace(self):
         """
         Handy function to print out the CPU state. You might want to call this
@@ -72,28 +106,17 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        PC = self.pc
-        halted = False
-        while not halted:
+        # PC = self.pc
+        while not self._halted:
             # Get the instruction from ram and store in local instruction register
-            IR = self.ram_read(PC)
+            IR = self.ram_read(self.pc)
             # Get operands
-            operand_a = self.ram_read(PC + 1)
-            operand_b = self.ram_read(PC + 2)
-            # Run the correct operation
-            if IR == 0b10000010:      # LDI
-                self.registers[operand_a] = operand_b
-                PC += 3
-            elif IR == 0b01000111:    # PRN
-                print(self.registers[operand_a])
-                PC += 2
-            elif IR == 0b10100010:    # MUL
-                product = self.registers[operand_a] * self.registers[operand_b]
-                self.registers[operand_a] = product
-                PC += 3
-            elif IR == 0b00000001:    # HLT
-                halted = True
-            else:
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            # Run the correct operation using the branch table
+            try:
+                self.branch_table[IR](operand_a, operand_b)
+            except:
                 raise Exception(f"Command {IR} does not exist")
 
 
