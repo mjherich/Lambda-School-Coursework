69a8f3b128fd915a74a7071e2749b02d66eda2d1
Matt Herich <mjherich@gmail.com>
2020-01-30 17:08:34 -0800

    Simple.py from lecture has bug

--- simple.py
+++ simple.py
@@ -8,6 +8,8 @@ PRINT_REGISTER  = 5  # Saves a value to a register
 ADD             = 6
 PUSH            = 7
 POP             = 8
+CALL            = 9
+RET             = 10
 
 # memory = [
 #     PRINT_BEEJ,
@@ -68,6 +70,7 @@ while running:
     # Execute
 
     command = memory[pc]
+    print(command)
 
     if command == PRINT_BEEJ:
         print("Beej!")
@@ -111,6 +114,26 @@ while running:
         registers[SP] += 1
         # Increment PC by 2
         pc += 2
+    elif command == CALL:
+        # The address of the instruction directly after CALL is pushed onto the stack.
+        val = pc + 2
+        registers[SP] -= 1
+        memory[registers[SP]] = val
+        # The PC is set to the address stored in the given register.
+        reg = memory[pc + 1]
+        subroutine_address = registers[reg]
+        print(f"CALLING SR AT ADDRESS {subroutine_address % 256}")
+        # We jump to that location in RAM and execute the first instruction in the subroutine.
+        # The PC can move forward or backwards from its current location.
+        pc = subroutine_address
+    elif command == RET:
+        # Return from subroutine.
+        # Pop the value from the top of the stack and store it in the PC.
+        return_address = registers[SP]
+        print(f"RETURNING TO ADDRESS {return_address % 256}")
+        pc = memory[return_address]
+        # Increment the SP by 1
+        registers[SP] += 1
     else:
         print(f"Error: Unkown command: {command}")
         sys.exit(1)
